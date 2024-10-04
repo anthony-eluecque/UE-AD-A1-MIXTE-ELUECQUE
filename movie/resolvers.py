6e4cbf1a3e43a56245df9bc73239a54fe6ba0109
Antorakk <95444282+Antorakk@users.noreply.github.com>
2024-10-04 23:21:58 +0200

    imp: clean code

--- movie/resolvers.py
+++ movie/resolvers.py
@@ -1,8 +1,28 @@
 import json
+from typing import TypedDict, Optional, List
 import uuid
 
-MOVIES_DB_PATH = "{}/data/movies.json"
-ACTORS_DB_PATH = "{}/data/actors.json"
+MOVIES_DB_PATH : str = "{}/data/movies.json"
+ACTORS_DB_PATH : str = "{}/data/actors.json"
+
+class MutationResponse(TypedDict):
+    success : bool
+    error :  Optional[str]
+
+class Movie(TypedDict):
+    id : str
+    title : str
+    director : str
+    rating : float
+
+class Actor(TypedDict):
+    id : str
+    firstname: str
+    lastname : str
+    birthyear : int 
+    films : List[str]
+
+
 class Json():
     
     @staticmethod
@@ -19,16 +39,17 @@ class Json():
         with open(path.format("."), "w") as wfile:
             json.dump(obj, wfile, indent=2)
 
-def movie_with_id(_,info,_id):
-    movies = Json.open(MOVIES_DB_PATH, "movies")
+def movie_with_id(_, info, _id : str) -> Movie | None:
+    movies : List[Movie] = Json.open(MOVIES_DB_PATH, "movies")
     for movie in movies:
         if movie['id'] == _id:
             return movie
+    return None
             
-def update_movie_rate(_,info,_id,_rate):
+def update_movie_rate(_, info, _id : str, _rate : float) -> Movie:
     newmovies = {}
-    newmovie = {}
-    movies = Json.open(MOVIES_DB_PATH, "movies")
+    newmovie : Movie
+    movies : List[Movie] = Json.open(MOVIES_DB_PATH, "movies")
     for movie in movies:
         if movie['id'] == _id:
             movie['rating'] = _rate
@@ -37,22 +58,23 @@ def update_movie_rate(_,info,_id,_rate):
     Json.write(MOVIES_DB_PATH, "movies", newmovies)
     return newmovie
 
-def delete_movie_from_id(_, info, _id : str): 
-    movies = Json.open(MOVIES_DB_PATH, "movies")
+def delete_movie_from_id(_, info, _id : str) -> MutationResponse: 
+    movies : List[Movie] = Json.open(MOVIES_DB_PATH, "movies")
     for movie in movies :
         if movie['id'] == _id:
             movies.remove(movie)
             Json.write(MOVIES_DB_PATH, "movies", movies)
             return {
-                "success": True
+                "success": True,
+                "error": None
             }
     return {
         "success": False,
         "error": f"Movie with id : {_id} not existing"
     }
 
-def create_movie(_, info, input):
-    new_movie = {
+def create_movie(_, info, input : Movie) -> MutationResponse:
+    new_movie : Movie = {
         "id": str(uuid.uuid4()),
         "title": input["title"],
         "rating": input["rating"],
@@ -60,7 +82,7 @@ def create_movie(_, info, input):
     }
 
     try:
-        movies = Json.open(MOVIES_DB_PATH, "movies")
+        movies : List[Movie] = Json.open(MOVIES_DB_PATH, "movies")
 
         for movie in movies:
             if movie["title"] == new_movie["title"]:
@@ -70,35 +92,39 @@ def create_movie(_, info, input):
                 }
         movies.append(new_movie)
         Json.write(MOVIES_DB_PATH, "movies", movies)
-        return {"success": True}
+        return {
+            "success": True, 
+            "error": None
+        }
     except Exception as err:
         return {
             "success": False,
             "error": str(err),
         }
 
-def movies_by_min_rate(_, info, _rate):
+def movies_by_min_rate(_, info, _rate : float) -> List[Movie]:
     return movies_by_field_with_conditions("rating", lambda rating: rating >= _rate)
 
-def movies_by_director(_, info, _director):
+def movies_by_director(_, info, _director : str) -> List[Movie]:
     return movies_by_field_with_conditions("director", lambda director: director == _director)
 
-def movie_with_title(_, info, _title):
-    movies = Json.open(MOVIES_DB_PATH, "movies")
+def movie_with_title(_, info, _title : str) -> Movie | None:
+    movies : List[Movie] = Json.open(MOVIES_DB_PATH, "movies")
     for movie in movies:
         if movie["title"] == _title:
             return movie
+    return None
 
-def movies_by_field_with_conditions(key, condition_func):
-    matching_movies = []
-    movies = Json.open(MOVIES_DB_PATH, "movies")
+def movies_by_field_with_conditions(key : str, condition_func) -> List[Movie]:
+    matching_movies : List[Movie] = []
+    movies : List[Movie] = Json.open(MOVIES_DB_PATH, "movies")
     for movie in movies:
         if condition_func(movie[key]):
             matching_movies.append(movie)
     return matching_movies
 
 
-def resolve_actors_in_movie(movie, info):
-    actors = Json.open(ACTORS_DB_PATH, "actors")
+def resolve_actors_in_movie(movie : Movie, info) -> List[Actor]:
+    actors : List[Actor] = Json.open(ACTORS_DB_PATH, "actors")
     result = [actor for actor in actors if movie['id'] in actor['films']]
     return result
